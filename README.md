ca772bf02df716255de893e99d12a689700959da
Travis LaDuke <travisladuke@gmail.com>
2018-08-09 17:24:47 -0700

    Re-did it

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # Spawn a ZeroTier
 Then you can talk to it's [json api](https://github.com/zerotier/ZeroTierOne/tree/master/controller) at `localhost:${port}/controller`
 
-See the tests
-
 ```javascript
 var path = require('path')
 var spawnController = require('./index')
@@ -16,18 +14,14 @@ var ztBinary = path.join(
 )
 
 var home = './tmp'
-var port = 19993
-var opts = { ztBinary: ztBinary, home: home, port: port }
+var opts = { ztBinary: ztBinary, home: home }
 
 spawnController(opts, function (err, controller) {
   if (err) throw err
+
+  var { proc, controllerId, authtoken, port } = controller
   
   // do stuff
 
-  controller.proc.kill()
 })
 ```
-
-## todo 
-- [ ] let it start in a linux network namespace if one is provided
-- [ ] it can talk over stdio, but i'm not that familiar with it yet; it's be cool to make some node streams.
--- spawn.js
+++ spawn.js
@@ -1,64 +1,163 @@
 var spawn = require('child_process').spawn
+var parallel = require('run-parallel')
+var series = require('run-series')
 var join = require('path').join
 var assert = require('assert')
+var http = require('http')
+var net = require('net')
 var fs = require('fs')
-var testPort = require('find-port')
 
-module.exports = function spawnController (opts, cb) {
-  spawner(opts, cb)
-}
+module.exports = spawner
 
 function spawner (opts, cb) {
   assert(opts.ztBinary, 'need path to a zerotier-one binary in options arg')
-  assert(opts.home, 'need path to a str dir in options arg')
-  assert(opts.port, 'need port in options arg')
+  assert(opts.home, 'need path to home dir in options arg')
 
-  var path = join(opts.home, 'zerotier-one.pid')
-
-  testPort('localhost', opts.port, opts.port, function (res) {
-    if (res.length === 0) {
-      var e = new Error('Something already running on port: ' + opts.port)
-      return cb(e)
-    }
-
-    fs.readFile(path, { encoding: 'utf8' }, function (_err, res) {
-      if (res) {
-        var e = new Error(
-          'ZeroTier one instance already running in working dir: ' + opts.home
-        )
-        return cb(e)
-      }
-
-      var proc = spawn(opts.ztBinary, ['-U', `-p${opts.port}`, opts.home], {
-        detached: false,
-        stdio: ['pipe', 'ipc', 'pipe'],
-        shell: false
+  parallel([
+    function (done) { checkPIDfile(opts.home, done) },
+    function (done) {
+      checkPortFile(opts.home, function (err, port) {
+        if (err) return done(null, { lastPort: null })
+        done(null, { lastPort: Number(port) })
       })
+    },
+    function (done) {
+      getRandomPort(function (err, randomPort) {
+        if (err) throw (err)
 
-      waitFile(join(opts.home, 'zerotier-one.pid'), function (err, pid) {
-        if (err) return cb(err)
+        done(null, { randomPort })
+      })
+    }
+  ],
+  function (err, res) {
+    if (err) throw (err)
 
-        waitFile(join(opts.home, 'zerotier-one.port'), function (err, pid) {
-          if (err) return cb(err)
+    var props = smoosh(res)
+    var port = props.lastPort || props.randomPort
 
-          cb(null, proc)
+    var proc
+
+    series([
+      function (done) {
+        proc = spawn(opts.ztBinary, ['-U', `-p${port}`, opts.home], {
+          detached: false,
+          shell: false
         })
+        done(null, { proc })
+      },
+      function (done) {
+        waitForHttp(port, function (err) {
+          done(err, { port })
+        })
+      },
+      function (done) {
+        getControllerId(opts.home, function (err, controllerId) {
+          done(err, { controllerId })
+        })
+      },
+      function (done) {
+        getAuthToken(opts.home, function (err, authToken) {
+          done(err, { authToken })
+        })
+      }
+    ], function (err, res) {
+      if (err) throw (err)
+
+      cb(null, smoosh(res))
+    })
+
+    process.on('uncaughtException', function (e) {
+      console.error(e)
+      proc.kill()
+      process.exit(1)
+    })
+
+    process.once('SIGUSR2', function () {
+      // helps nodemon
+      proc.kill()
+      proc.on('close', function () {
+        process.kill(process.pid, 'SIGUSR2')
       })
     })
   })
 }
 
-function waitFile (path, cb, count) {
-  if (!count) count = 0
-  if (count > 500) throw new Error('Waited too long for file to appear ' + path)
+function checkPIDfile (home, cb) {
+  var path = join(home, 'zerotier-one.pid')
+
+  fs.readFile(path, { encoding: 'utf8' }, function (_err, res) {
+    if (res) {
+      var e = new Error(
+        'ZeroTier one instance already running in working dir: ' + home
+      )
+      return cb(e)
+    } else {
+      return cb(null, true)
+    }
+  })
+}
+
+function checkPortFile (home, cb) {
+  var path = join(home, 'zerotier-one.port')
 
   fs.readFile(path, { encoding: 'utf8' }, function (err, res) {
-    if (err) {
-      setTimeout(function () {
-        waitFile(path, cb, count + 1)
-      }, 10)
-    } else {
-      cb(null, res)
+    if (err) return cb(null, null)
+
+    return cb(null, res)
+  })
+}
+function getRandomPort (cb) {
+  var server = net.createServer()
+  server.unref()
+  server.on('error', cb)
+
+  server.listen(0, function () {
+    var port = server.address().port
+    server.close(function () {
+      cb(null, port)
+    })
+  })
+}
+
+function waitForHttp (port, cb) {
+  setTimeout(main, 10)
+
+  function main () {
+    var request = http.request({ port }, function (response) {
+      if (response) {
+        cb()
+      }
+    })
+
+    request.on('error', function (e) {
+      retry()
+    })
+    request.end()
+
+    function retry () {
+      setTimeout(main, 200)
     }
+  }
+}
+
+function getControllerId (home, cb) {
+  fs.readFile(home + '/identity.public', 'utf8', function (err, res) {
+    if (err) { return cb(err) }
+
+    cb(null, res.split(':')[0])
+  })
+}
+
+function getAuthToken (home, cb) {
+  fs.readFile(home + '/authtoken.secret', 'utf8', function (err, res) {
+    if (err) { return cb(err) }
+
+    cb(null, res)
+  })
+}
+
+function smoosh (arr) {
+  return arr.reduce(function (acc, el) {
+    return Object.assign({}, acc, el)
   })
 }
